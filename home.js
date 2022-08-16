eadfa4e59c2fc3f1d0dee340eb597ae82a9f4177
Daphne Benzaquen <98908807+dbenzaquen89@users.noreply.github.com>
2022-08-16 15:12:07 -0500

    added files from deployed site

---
+++ home.js
@@ -0,0 +1,19 @@
+function Home(){
+  return (
+
+<Card
+      bgcolor="info"
+      txtcolor= "white"
+      header="Trust Us with All Your Financial Needs...NOT"
+      title="Welcome to Easiest Bank to Hack"
+      text="Explore the site using the Navigation Bar."
+      body={(<img src="bank.png" className="img-fluid" alt="image of bank"
+     
+      />
+      
+      
+      )}
+      
+    />    
+  );  
+}
---
+++ navbar.js
@@ -0,0 +1,41 @@
+function NavBar(){
+  return(
+    <>
+<nav className="navbar navbar-expand-lg navbar-light background color-">
+      <a className="navbar-brand" 
+      href="#">The Easiest Bank to Hack</a>
+      <button className="navbar-toggler" 
+      type="button" 
+      data-toggle="hover" 
+      data-target="#navbarNav" 
+      aria-controls="navbarNav" 
+      aria-expanded="false" 
+      aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        
+        
+        <ul className="navbar-nav">
+        <li className="nav-item">
+            <a className="nav-link" href="#/">Home </a>
+          </li> 
+          <li className="nav-item">
+            <a className="nav-link" href="#/CreateAccount/">Create Account</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/deposit/">Deposit</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/withdraw/">Withdraw</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/alldata/">AllData</a>
+          </li>       
+          
+        </ul>
+      </div>
+    </nav>
+    </>
+  );
+}
